56a063bf83f1880f10e5606481422db1eb826b3a
antoniocentenocampos <centeno.campos@bootcamp-upgrade.com>
2021-02-13 13:57:34 +0100

    timeline

--- src/Pages/ChronologyPage/ChronologyPage.jsx
+++ src/Pages/ChronologyPage/ChronologyPage.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { API } from '../../shared/consts/api.consts';
 
-export function ChronologyPage() {
-  return (
+export function ChronologyPage () {
+
+    const [timeline, setTimeline] = useState([]);
+
+    const getChronology = () => {
+        
+        API.get('/show/characters').then((res) => {
+            setTimeline(res.data)
+            console.log(res.data); 
+        });
+    }
+
+    useEffect(getChronology, []);
+
+    return (
     <div>
-      <h1>WELCOME TO CHRONOLOGY</h1>
+        {timeline.map((item, i) =>
+
+          <div key={i}>
+            {/* <p>{item.age}</p> */}
+            <p>{item.name}</p>
+            <img src="{item.image}"/>
+          </div>
+        )}  
     </div>
-  );
+    )
 }
