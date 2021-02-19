e58f07664bad6f3502902bfeadf377edf1509c9b
i-am-Claratj <clara@claratoloba.com>
2021-02-19 18:04:31 +0100

    puliendo los estilos

--- src/Pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
+++ src/Pages/CharactersPage/components/CharactersGallery/CharactersGallery.jsx
@@ -1,34 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
+import './CharactersGallery.scss';
+
 
 
 export function CharactersGallery(props) {
 
-    // const App = () => (
-    //     <SimpleBar style={{ maxHeight: 300 }}>
-
-    //     </SimpleBar>
-    // );
-
-
-
     return (
 
-        <div className="row">
-            {/* <SimpleBar style={{ maxHeight: 300 }}> */}
+        <div className="row gallery">
             {props.characters.map((character, i) =>
                 <div className="col-12 col-sm-6 col-md-4 col-lg-3">
                     <Link to={"/characters/" + character.name}>
-                        <figure key={i}>
-                            <img src={character.image} alt={character.name} />
+                        <figure key={i} className="character-gallery" >
+                            <img className="character-gallery__img" src={character.image} alt={character.name} />
+                            <figcaption className="character-gallery__figcaption">{character.name}</figcaption>
                         </figure>
-                        <figcaption>{character.name}</figcaption>
                     </Link>
                 </div>
             )};
-            {/* </SimpleBar> */}
         </div>
     );
 }
