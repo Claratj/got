31e01ccbd9eeb4c54b66a1fca19f133759f867b2
antoniocentenocampos <centeno.campos@bootcamp-upgrade.com>
2021-03-18 13:42:40 +0100

    cronology okey

--- src/Pages/ChronologyPage/ChronologyPage.jsx
+++ src/Pages/ChronologyPage/ChronologyPage.jsx
@@ -4,10 +4,9 @@ import { LoadingContext } from '../../core/components/Loading/contexts/LoadingCo
 import { Header } from '../../core/components/Header/Header';
 import { Footer } from '../../core/components/Footer/Footer';
 import { useTranslation } from 'react-i18next';
-import './Chronology.scss';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-
+import './Chronology.scss';
 
 export function ChronologyPage(props) {
 
@@ -89,9 +88,9 @@ export function ChronologyPage(props) {
   return (
     <div className="gallery-container">
       <Header house={true} fnLanguage={changeLanguage} />
-      <span onClick={reverseTimelineOrder} className={ascendingOrder ? "icon-circle-up b-icon b-icon--circle" :
+      {/* <span onClick={reverseTimelineOrder} className={ascendingOrder ? "icon-circle-up b-icon b-icon--circle" :
         "icon-circle-down b-icon b-icon--circle"}>
-      </span>
+      </span> */}
       {/*<div className="gallery gallery_chr">
         <div className="chronology">
 
@@ -114,17 +113,20 @@ export function ChronologyPage(props) {
 
       <div className="gallery gallery_chr">
       <div className="chronology">
+      
+      <span onClick={reverseTimelineOrder} className={ascendingOrder ? "icon-circle-up b-icon b-icon--circle" :
+        "icon-circle-down b-icon b-icon--circle"}>
+      </span>
         <VerticalTimeline>
 
           {timeline.map((item, i) =>
 
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              contentStyle={{ background: 'rgb(0, 0, 0)', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '0px solid  rgb(0, 0, 0)' }}
+              contentStyle={{ "background-color": '#000', color: '#fff' }}
+              contentArrowStyle={{ borderRight: '0px solid  #fff' }}
               iconStyle={{ background: 'rgb(0, 0, 0)', color: '#fff' }}
-            >
-          
+            >          
               <p>{item.age && item.age.age}</p>
               <p>{item.name}</p>
               <img className="chronology__card__img" src={item.image} alt={item.name} />
